5fb5133c7a1787866fd20c3e67509dc057f3671b
ahmed <Kharbouch_ahmed@hotmail.com>
2023-07-08 01:02:39 +0100

    first commit

---
+++ supplier-service/src/main/java/com/example/Fournisseurservice/entities/Fournisseur.java
@@ -0,0 +1,28 @@
+package com.example.Fournisseurservice.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Fournisseur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nom;
+    String telephone;
+    String telephoneFixe;
+    String email;
+    String adresse;
+    String ville;
+    Date createdAt;
+    Date modifiedAt;
+    @ManyToOne
+    TypeFournisseur typeFournisseur;
+}
+
---
+++ supplier-service/src/main/java/com/example/Fournisseurservice/repositories/FournisseurRepository.java
@@ -0,0 +1,18 @@
+package com.example.Fournisseurservice.repositories;
+
+import com.example.Fournisseurservice.entities.Fournisseur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Collection;
+
+
+public interface FournisseurRepository extends JpaRepository<Fournisseur,Long> {
+
+   Fournisseur findFournisseurById(Long id);
+    Fournisseur findFournisseurByNom(String nom);
+
+    @Query("select c from Fournisseur c where c.nom like :kw")
+    Collection<Fournisseur> findFournisseurByName(@Param(value = "kw")String nom);
+}
